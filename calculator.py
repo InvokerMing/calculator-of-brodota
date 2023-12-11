54eabaac6bc92fd6d8658a5df139a8e88ec673ee
InvokerMing <a812980948@163.com>
2023-12-11 20:06:13 +0800

    v1.1
    新增切换龟龟

--- calculator.py
+++ calculator.py
@@ -31,24 +31,16 @@ def calc(x=1000, x_salary=0, price=0, salary=0, round=1, guigui=True):
         return y3 - y4
 
 
-def get_key_state(key):
-    if keyboard.is_pressed(key):
-        return "按下"
-    else:
-        return "未按下"
-
-
 gap = 275
 round = 0
 salary = 0
 money = 0
 ocr = CnOcr()
+guigui = True
 print("准备就绪")
 
 while True:
-    key_state_f2 = get_key_state("f2")
-    key_state_f3 = get_key_state("f3")
-    if key_state_f2 == "按下":
+    if keyboard.is_pressed("f2"):
         image = ImageGrab.grab()
         bbox_salary = [400, 715, 500, 745]
         bbox_round = [1633, 981, 1808, 1027]
@@ -88,46 +80,60 @@ while True:
                     result = str(output["text"])
                     price = int(result)
                     pro80 = calc(
-                        x=money, x_salary=salary, price=price, salary=80, round=round
+                        x=money,
+                        x_salary=salary,
+                        price=price,
+                        salary=80,
+                        round=round,
+                        guigui=guigui,
                     )
                     pro270 = calc(
-                        x=money, x_salary=salary, price=price, salary=270, round=round
+                        x=money,
+                        x_salary=salary,
+                        price=price,
+                        salary=270,
+                        round=round,
+                        guigui=guigui,
                     )
                     print(f"检测到牧师，扣除价格，80投资最终将带来{pro80}收益，270投资最终将带来{pro270}收益")
             except:
-                print(f"第{i+1}个英雄没检测到")
+                print(f"第{i+1}个英雄检测失败")
             bbox_hero[0] = bbox_hero[0] + gap
             bbox_hero[2] = bbox_hero[2] + gap
             bbox_hero_price[0] = bbox_hero_price[0] + gap
             bbox_hero_price[2] = bbox_hero_price[2] + gap
 
         for i in range(0, 4):
-            # try:
-            image_item = image.crop(box=bbox_item)
-            out = ocr.ocr(image_item)
-            for output in out:
-                result = str(output["text"])
-                # print(f"{i+1}, {result}")
-                if "投资" in result:
-                    i_salary = extract_number(result)
-                    image_item_price = image.crop(box=bbox_item_price)
-                    outd = ocr.ocr(image_item_price)[0]
-                    price = int(str(outd["text"]))
-                    pro = calc(
-                        x=money,
-                        x_salary=salary,
-                        price=price,
-                        salary=i_salary,
-                        round=round,
-                    )
-                    print(f"检测到投资物品，扣除价格，{i_salary}投资最终将带来{pro}收益")
-            # except:
-            #     print(f"第{i+1}个物品没检测到")
+            try:
+                image_item = image.crop(box=bbox_item)
+                out = ocr.ocr(image_item)
+                for output in out:
+                    result = str(output["text"])
+                    # print(f"{i+1}, {result}")
+                    if "投资" in result:
+                        i_salary = extract_number(result)
+                        image_item_price = image.crop(box=bbox_item_price)
+                        outd = ocr.ocr(image_item_price)[0]
+                        price = int(str(outd["text"]))
+                        pro = calc(
+                            x=money,
+                            x_salary=salary,
+                            price=price,
+                            salary=i_salary,
+                            round=round,
+                            guigui=guigui,
+                        )
+                        print(f"检测到投资物品，扣除价格，{i_salary}投资最终将带来{pro}收益")
+            except:
+                print(f"第{i+1}个物品检测失败")
             bbox_item[0] = bbox_item[0] + gap
             bbox_item[2] = bbox_item[2] + gap
             bbox_item_price[0] = bbox_item_price[0] + gap
             bbox_item_price[2] = bbox_item_price[2] + gap
         print("-----------------------------------------------------------")
-    if key_state_f3 == "按下":
+    if keyboard.is_pressed("f3"):
         print("程序正在退出")
         sys.exit()
+    if keyboard.is_pressed("f4"):
+        guigui = not guigui
+        print(f"是否为龟龟：{guigui}")
